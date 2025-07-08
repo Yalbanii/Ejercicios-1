921e4b9cc83120863c08ab819d537de6d7d42817
Yalbani Aranda <yarandacardona@gmail.com>
2025-07-07 18:01:31 -0600

    add styles

--- codigo-otros-1/assets/js/app.js
+++ codigo-otros-1/assets/js/app.js
@@ -1,23 +1,25 @@
 const baseEndpoint = 'https://api.github.com';
 const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('name');
-const $b = document.querySelector('#blog');
+const $n = document.querySelector('.name');
+//agregar el . para que el link a la API funcione.
+const $b = document.querySelector('.blog');
 const $l = document.querySelector('.location');
 
-//ASYNC FUNCTION
+//agregar ASYNC FUNCTION
  async function displayUser(username) {
   $n.textContent = 'cargando...';
   const response = await fetch (`${usersEndpoint}/${username}`);
+    const data = await response.json(); //
   console.log(data);
-  $n.textContent = '${data.name}';
-  $b.textContent = '${data.blog}';
-  $l.textContent = '${data.location}';
+  $n.textContent = `${data.name}`;
+  $b.textContent = `${data.blog}`;
+  $l.textContent = `${data.location}`;
 }
 
 function handleError(err) {
   console.log('OH NO!');
   console.log(err);
-  n.textContent = `Algo salió mal: ${err}`
+  n.textContent = `Algo salió mal: ${err}`;
 }
 
 displayUser('stolinski').catch(handleError);
